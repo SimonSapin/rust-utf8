6606e23fae100fff3d0006bce2f6c70daad43b81
Simon Sapin <simon.sapin@exyr.org>
2015-10-02 13:54:58 +0200

    Fix an edge case of string_from_utf8_lossy and add a test

--- tests/string_from_utf8_lossy.rs
+++ tests/string_from_utf8_lossy.rs
@@ -8,20 +8,24 @@ mod data;
 
 /// A re-implementation of String::from_utf8_lossy
 pub fn string_from_utf8_lossy(input: &[u8]) -> Cow<str> {
+    let mut string;
     let mut decoder = Decoder::new();
-    let mut iter = decoder.feed(input);
-    // The first piece is special: we want to return Cow::Borrowed if possible.
-    match iter.next() {
-        Some(DecodedPiece::InputSlice(ref s)) if iter.eof() => (*s).into(),
-        None => "".into(),
-        Some(first) => {
-            let mut string = first.to_owned();
-            for piece in iter {
-                string.push_str(&piece)
-            }
-            string.into()
+    {
+        let mut iter = decoder.feed(input);
+        // The first piece is special: we want to return Cow::Borrowed if possible.
+        match iter.next() {
+            None => return "".into(),
+            Some(DecodedPiece::InputSlice(ref s)) if iter.eof() => return (*s).into(),
+            Some(first) => string = first.to_owned(),
+        };
+        for piece in iter {
+            string.push_str(&piece)
         }
     }
+    if let Some(piece) = decoder.end() {
+        string.push_str(&piece)
+    }
+    string.into()
 }
 
 #[test]
